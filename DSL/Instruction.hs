3a1526d3e7280da63b55f19f9313c3a14e655a59
Samuel A. Yallop <syallop@gmail.com>
2014-12-08 03:29:06 +0000

    Adds Instruction element relation
    - Adds instruction element relation constraint 'i :<- j' declaring that
      'i' either is 'j', or it is part of 'j's composition (via :+:).

--- DSL/Instruction.hs
+++ DSL/Instruction.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE  GADTs
+{-# LANGUAGE FlexibleInstances
+            , GADTs
+            , MultiParamTypeClasses
             , TypeOperators
   #-}
 module DSL.Instruction
   ( (:+:)(InjL,InjR) -- Instruction composition
+  , (:<-)(inj)       -- Element instructions
   ) where
 
 -- | Composition of two instruction types 'i' and 'j'
@@ -12,4 +15,22 @@ data (i :+: j) p a where
   InjL :: i p a -> (i :+: j) p a
   InjR :: j p a -> (i :+: j) p a
 infixr :+:
+
+-- | Class of sub-instructions.
+-- 'i' is/ is contained within 'i\''.
+class i :<- i' where
+  inj :: i p a -> i' p a -- ^ Inject a term in 'i' into a term in 'i\''.
+
+-- Instructions are trivially themselves
+instance i :<- i where
+  inj = id
+
+-- Instruction contained in left side of a composition.
+instance i :<- (i :+: i') where
+  inj = InjL
+
+-- Instruction appears somewhere within the right side of a composition.
+instance (i :<- i'')
+      => i :<- (i' :+: i'') where
+  inj = InjR . inj
 
