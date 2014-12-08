ef42fce74c6fed6422c6b1adb6885b22da7d100c
Samuel A. Yallop <syallop@gmail.com>
2014-12-08 03:56:34 +0000

    Adds 'Program' a concrete monadic sequence of instructions

---
+++ DSL/Program.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE GADTs #-}
+module DSL.Program
+  ( Program(..)
+  , MonadInstr(..)
+  ) where
+
+import DSL.Instruction
+
+-- | Concrete representation of a monadic sequence of
+-- instructions.
+data MonadInstr i m a where
+
+  -- | Insert an instruction into the context.
+  Instr  :: i m a -> MonadInstr i m a
+
+  -- | Monad 'return'.
+  Return :: a -> MonadInstr i m a
+
+  -- | Monad 'bind' / '>>='.
+  Bind :: m a -> (a -> m b) -> MonadInstr i m b
+
+-- | A Program is a monadic sequence of instructions 'i' which can
+-- be interpreted to produce some return value 'a'.
+newtype Program i a = Program (MonadInstr i (Program i) a)
+
+instance Monad (Program i) where
+  return   = Program . Return
+  ma >>= f = Program $ Bind ma f
+
